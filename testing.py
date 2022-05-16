94a335ed7fe97fca5acba1ab8af25318f43fb07d
yyt0702 <105671462+yyt0702@users.noreply.github.com>
2022-05-17 02:44:24 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,6 +1,5 @@
 import requests
  
-# Making a get request
 
 url = "https://beta.neutrino.at/api/explorer"
 total_issue = url + "/get_total_issued"
@@ -14,8 +13,6 @@ ts = requests.get(total_issue)
 br = requests.get(backing_ratio)
 df = requests.get(deficit)
 
-
-# print json content
 print("circulating supply: " , cs.json())
 print("total supply: " , ts.json())
 print("backing ratio: " , br.json())
