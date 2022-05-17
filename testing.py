466c74208b88d34891a5c44c95809a9d511947fe
yyt0702 <105671462+yyt0702@users.noreply.github.com>
2022-05-17 11:31:05 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 import requests
- 
+import pandas as pd  
 
 url = "https://beta.neutrino.at/api/explorer"
 total_issue = url + "/get_total_issued"
@@ -17,3 +17,12 @@ print("circulating supply: " , cs.json())
 print("total supply: " , ts.json())
 print("backing ratio: " , br.json())
 print("deficit: ", df.json())
+
+
+
+
+
+url = "https://api.vires.finance/state"
+res = requests.get(url).json() 
+df = pd.DataFrame(res)
+df.to_csv("vires.csv")
